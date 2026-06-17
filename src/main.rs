1214b35da632c3b2a52bc316a1ce8278a3d58db1
Srinithy67 <srinithy63@gmail.com>
2026-06-17 14:43:16 +0530

    hello name backend in rust using axum framework

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
 use axum::{
-    extract::Path,
     routing::get,
     Router,
 };
 
-async fn hello_name(Path(name): Path<String>) -> String {
-    format!("Hello {}", name)
-}
-
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/hello/{name}", get(hello_name));
+        .route("/", get(|| async { "Hello Sri" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
-
-    println!("Server running on http://localhost:3000");
 
     axum::serve(listener, app)
         .await

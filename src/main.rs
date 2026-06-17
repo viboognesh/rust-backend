b5a6d1e278c5c52ba5d006b6cf28339a32826d30
Srinithy67 <srinithy63@gmail.com>
2026-06-17 14:54:58 +0530

    Accept name as input and return greeting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use axum::{
+    extract::Path,
     routing::get,
     Router,
 };
 
+async fn hello_name(Path(name): Path<String>) -> String {
+    format!("Hello {}", name)
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "Hello Sri" }));
+        .route("/hello/{name}", get(hello_name));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
